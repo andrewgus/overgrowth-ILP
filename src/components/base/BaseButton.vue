ed4687e6e01160ebbba3f65811b29de2d414cd15
andrewgus <tandrewgus@gmail.com>
2023-01-10 09:27:51 -0500

    Hydration bug fixed

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+	<button v-if="!link">{{ text }}</button>
+	<a v-else :href?="{ href }"></a>
+</template>
+
+<script setup lang="ts">
+	defineProps({
+		text: {
+			type: String,
+			required: true,
+		},
+		link: {
+			type: Boolean,
+		},
+		href: {
+			type: String,
+		},
+	})
+</script>
+
+<style scoped></style>
