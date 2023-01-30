70585bad1f8b9745353178508bb3583dd56873f2
andrewgus <tandrewgus@gmail.com>
2023-01-30 14:03:42 -0500

    Fixed typo

--- src/composables/UseComputedCssModule.ts
+++ src/composables/UseComputedCssModule.ts
@@ -2,13 +2,13 @@ import { ComputedRef, useCssModule, computed } from 'vue'
 /**
  * To be used when applying multiple conditional classes with CSS modules
  * @param baseClass class that is always applied
- * @param conditionalClassesMap Map of reactive boolean values to be met & classes to be applied based on them
+ * @param conditionalClassesMap Map of reactive boolean values to be met & classes to be applied based on each
  * @param moduleName Optional name for the css module
  * @returns array of classes to be applied to an element
  */
 export default function useComputedCssModule(
 	baseClass: string,
-	conditionClassesMap: Map<ComputedRef<boolean>, string>,
+	conditionalClassesMap: Map<ComputedRef<boolean>, string>,
 	moduleName?: string
 ) {
 	const styles = useCssModule(moduleName)
@@ -16,7 +16,7 @@ export default function useComputedCssModule(
 	const computedStyles = computed(() => {
 		const stylesArr: string[] = [styles[baseClass]]
 
-		for (const [condition, styleClass] of conditionClassesMap.entries()) {
+		for (const [condition, styleClass] of conditionalClassesMap.entries()) {
 			if (condition.value) stylesArr.push(styles[styleClass])
 		}
 		return stylesArr
