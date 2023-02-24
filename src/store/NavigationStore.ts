a84f305b59185d131b9b5dac8d67f24ab04fd0c5
andrewgus <tandrewgus@gmail.com>
2023-02-24 15:38:56 -0500

    More progress

--- src/store/NavigationStore.ts
+++ src/store/NavigationStore.ts
@@ -1,4 +1,4 @@
-import { atom, map, computed } from 'nanostores'
+import { atom, map, computed, cleanTasks } from 'nanostores'
 import { featuresMap } from './FeatureSettingsStore'
 import type { FeatureType } from './FeatureSettingsStore'
 
@@ -39,7 +39,7 @@ const useSetCurrSection = (sectionKey: string) => {
 	}
 }
 
-// filtered sections determined if features are turned on or off
+// filtered sections items that should be visible and navigable
 const filteredNavSectionsComputed = computed(
 	[allSectionsMap, featuresMap],
 	(allSections, features) => {
@@ -49,50 +49,59 @@ const filteredNavSectionsComputed = computed(
 		const filterForUnlocked = allSectionsAsArray.filter(([_, details]) => {
 			const isFeatureOn = features[details.isFeatureType as FeatureType]
 
-			// Must be either 1) static content or feature must be true AND 2) section is unlocked
+			//Finding the next active feature...
+			const findNextActiveFeatureIndex = allSectionsAsArray.findIndex(
+				([_, details]) => {
+					return (
+						!!details.isFeatureType &&
+						features[details.isFeatureType as FeatureType]
+					)
+				}
+			)
+
+			if (isFeatureOn === false) {
+				// If the feature is off, unlock any content that comes before it.
+				allSectionsAsArray.forEach(([_, details]) => {
+					if (details.orderNum! <= findNextActiveFeatureIndex) {
+						details.isLocked = false
+					}
+				})
+			} else if (isFeatureOn === true) {
+				// If the feature is on, lock any content that comes after it.
+				allSectionsAsArray.forEach(([_, details]) => {
+					if (details.orderNum! > findNextActiveFeatureIndex) {
+						details.isLocked = true
+					}
+				})
+			}
+
+			// NOTE: MAY NEED TO SOMEHOW UPDATE THE ORDER NUM HERE. MAYBE NOT?
+			if (
+				(details.isFeatureType === false || isFeatureOn === true) &&
+				details.isLocked === false
+			) {
+				console.log(details)
+			}
+
+			// Must be either 1) static content or feature must be true (on) AND 2) section is unlocked
 			return (
 				(details.isFeatureType === false || isFeatureOn === true) &&
 				details.isLocked === false
 			)
 		})
-		// updating orderNum to match new order
-		// NOTE: This may have to move to the bottom, after the next filter, AFTER the two arrays are already concat, on the concat array, rather than the filterForUnlocked array.
-		filterForUnlocked.forEach(([_, details], i) => {
-			details.orderNum = i
-		})
-
-		// NOTE: If user turns off a Feature, then any proceeding sections, up until and including the next available Feature, that is on, is changed to isLocked: false.
-		/*  NOTE: If this works, will also need to consider what happens when a user starts the lesson, begins to unlock content, and then goes back to the top and switched features on/off. 
-				1. Should reset any completed features? Yes, but with a warning pop-up box first to confirm.
-				2. Should unlock any content trapped behind locked features? Yes. 
-		*/
-		allSectionsAsArray.forEach(([_, details], i) => {
-			// if features[feature] is false, as is filtered out above.
-			// Also, here, if features[feature] is false, unlock all static content until the next feature where features[feature] is true. If there are multiples of the same type, they will be filtered out, and this rule should still apply.
-			const isFeatureOn = features[details.isFeatureType as FeatureType]
-
-			// NOTE: if the feature is off, find all the static content with an orderNum higher than that feature, and switch to isLocked: false, up until we hit something that matches (!!details.isFeatureType && isFeatureOn)
-			// To above, another way to think about it is: find the next feature that matches (!!details.isFeatureType && isFeatureOn) and then turn anything below that to isLocked: false.
-			if (isFeatureOn === false) {
-				console.log(allSectionsAsArray[i])
-				console.log(details.isFeatureType)
-				console.log(details.id)
-				console.log(details.orderNum)
-			}
-		})
-		
-
-		// NOTE: Current thought process is to continue doing as above, and now also filter for the items that match what is stated on lines 69 & 70. Then, concat that array with the filterForUnlocked array and return that Object.fromEntries(concatArray)
 
 		return Object.fromEntries(filterForUnlocked)
 	}
 )
 
+// filtered section items that should be invisible
 const filteredLockedSectionsComputed = computed(
-	allSectionsMap,
-	(allSections) => {
+	[allSectionsMap, featuresMap],
+	(allSections, features) => {
 		const allSectionsAsArray: [string, SectionDetails][] =
 			Object.entries(allSections)
+
+		// TODO: Need to filter out any features that are turned
 
 		const filterForLocked = allSectionsAsArray.filter(([_, details]) => {
 			return details.isLocked === true
