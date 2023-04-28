1bf38862040fe5065a1cd8064faaf59467278397
andrewgus <tandrewgus@gmail.com>
2023-04-28 12:37:28 -0400

    Ongoing dialog work

--- src/components/localStorageAlert/localStorageDialog.vue
+++ src/components/localStorageAlert/localStorageDialog.vue
@@ -1,5 +1,5 @@
 <template v-if="doesLocalStorageExist">
-	<dialog ref="dialogEl" :class="$style.dialog" open>
+	<dialog ref="dialogEl" :class="$style.dialog" :open="doesLocalStorageExist">
 		<h2>Hey there!</h2>
 		<p>
 			Looks like you have some saved work on this lesson. Would you like to
@@ -10,7 +10,8 @@
 				text="Continue lesson?"
 				@btnClick="continueAndClose"
 			></BaseButton>
-			<BaseButton text="Reset lesson" @btnClick="resetAndClose"></BaseButton>
+			<!-- TODO: create a warning button prop to change the styles of this and any other warning btn. Consider primary, secondary, tert buttons instead? -->
+			<BaseButton text="Reset lesson?" @btnClick="resetAndClose"></BaseButton>
 		</div>
 	</dialog>
 </template>
@@ -28,11 +29,13 @@
 	const doesLocalStorageExist = ref<boolean>()
 	onMounted(() => (doesLocalStorageExist.value = !!getLocalStorage()))
 
-	const continueAndClose = () => {}
+	const continueAndClose = () => {
+		dialogEl.value!.close() as HTMLDialogElement
+	}
 	const resetAndClose = () => {
 		// FIXME: these actions below work as intended, but it is **NOT** reactive. Need to fix so the proper store items are also updated. Need to somehow watch the localStorage for changes too here.
 		localStorage.removeItem(lessonID.value)
-		dialogEl.value!.close() as HTMLDialogElement
+		continueAndClose()
 	}
 </script>
 
@@ -40,18 +43,22 @@
 	@use '../../styles/mixins.scss';
 	.dialog {
 		position: fixed;
+		display: flex;
+		flex-flow: column nowrap;
+		align-items: center;
 		width: clamp(340px, 96vw, 800px);
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
 		z-index: 999;
+		box-shadow: var(--s-6) var(--s-6) var(--s10) var(--yellow-5),
+			calc(-1 * var(--s-6)) calc(-1 * var(--s-6)) var(--s10) var(--yellow-5);
 		@include mixins.alert();
 
 		&::backdrop {
 			background-color: hsla(0, 0%, 98%, 0.5);
 		}
 		> h2 {
-			margin-left: var(--s2);
 			margin-top: 0;
 		}
 
